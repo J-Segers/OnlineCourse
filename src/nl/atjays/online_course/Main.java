fbb9d3ba2858b482351a8e27151edb3d528e1edb
J-Segers <jethroseger@gmail.com>
2022-03-07 12:59:26 +0100

    Hello, World!

--- src/nl/atjays/online_course/Main.java
+++ src/nl/atjays/online_course/Main.java
@@ -2,7 +2,12 @@ package nl.atjays.online_course;
 
 public class Main {
 
+    //The main method is the starting point of any application.
+    //An application can have only one entry point.
     public static void main(String[] args) {
+
+        //standerd when learning programming is to write code that outputs hello world.
+        System.out.println("Hello, World!");
 
     }
 
