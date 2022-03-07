c2dbbfd9ad47c021b5706c573f2d7fbafd9d9016
J-Segers <jethroseger@gmail.com>
2022-03-07 13:11:00 +0100

    my first variable

--- src/nl/atjays/online_course/Main.java
+++ src/nl/atjays/online_course/Main.java
@@ -6,8 +6,21 @@ public class Main {
     //An application can have only one entry point.
     public static void main(String[] args) {
 
-        //standerd when learning programming is to write code that outputs hello world.
+        //The standard when learning programming is to write code that outputs hello world.
+        //the following line prints Hello, World! to the console.
         System.out.println("Hello, World!");
+
+        //Variables
+        //Variables can consist of lots of different DataTypes (DataTypes are Keywords in Java.
+
+        //int is the type / DataType of the variable in this case an integer / whole number.
+        //After specifying the DataType you give it a name here called myFirstNumber.
+        //And after that you initialize the variable by giving it a value. The value of this variable is 5.
+        int myFirstNumber = 5;
+        //This entire line is a statement or a declaration statement to be precise.
+
+        //the following line prints the value of the the variable myFirstNumber to the console.
+        System.out.println(myFirstNumber);
 
     }
 
